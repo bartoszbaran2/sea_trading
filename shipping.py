49783919ad4964420afeb23a5346674be2b401b3
Bartosz Baran <bartosz.baran192@gmail.com>
2022-11-21 22:20:50 +0100

    multiple contructors and cass methods

--- shipping.py
+++ shipping.py
@@ -1,33 +1,38 @@
+import iso6346
+
+
 class ShippingContainer:
 
     next_serial = 1337
 
-    @staticmethod
-    def _generate_serial():
-        result = ShippingContainer.next_serial  # żeby zaczynało od 0 id
-        ShippingContainer.next_serial += 1
+    @classmethod
+    def _generate_serial(cls):
+        result = cls.next_serial  # żeby zaczynało od 0 id
+        cls.next_serial += 1
         return result
+
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(owner_code=owner_code, serial=str(serial).zfill(6))
+
+    @classmethod
+    def create_empty(cls, owner_code):  # gwarantuje, że nie będzie parametru contents
+        return cls(owner_code, contents=[])
+
+    @classmethod
+    def create_with_items(cls, owner_code, items):
+        return cls(owner_code, contents=list(items))  # automatycznie zamienia na listę
 
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code
         self.contents = contents
         # self.next_serial = self.next_serial  # RHS, odczyt, nie używać tak
-        self.serial = ShippingContainer._generate_serial()
+        # self.serial = ShippingContainer._generate_serial()
+        self.bic = ShippingContainer._make_bic_code(
+            owner_code=owner_code,
+            serial=ShippingContainer._generate_serial()
+        )
 
 
-
-
-
-
-
-
-
-sc = ShippingContainer(42, 'drugs')
-sc2 = ShippingContainer(42, 'drugs')
-print(sc.serial)
-print(sc2.serial)
-# print(sc.next_serial)
-# print(sc2.next_serial)
-# ShippingContainer.next_serial = 42  # przez class, wpływa na wszystkie obiekty
-# print(sc.next_serial)
-# print(sc2.next_serial)
+sc = ShippingContainer('ELO', ['drugs'])
+print(sc.bic)
